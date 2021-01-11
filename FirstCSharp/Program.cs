dca30bcff2dc9d050b6ce72ddfab0ca2a317fe46
Administrator <admin@example.com>
2021-01-11 15:53:51 -0500

    first C# commit

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string color = "white";
+            // string sketchyPlace = $"{color} unmarked van";
+            // Console.WriteLine(sketchyPlace); // white unmarked van
+            // Console.WriteLine("My name is {0}, I am " + 28 + " years old", "Tim"); // My name is Tim, I am 28 years old
+            // Console.WriteLine($"My name is {0}, I am " + 28 + " years old", "Tim"); // My name is 0, I am 28 years old
+
+            // // for loop from 1 to 5 including 5
+            // for (int i = 1; i <= 5; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // // loop from 1 to 5 excluding 5
+            // for (int i = 1; i < 5; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // // while loop from 1 to 5 including 5
+            // int i = 1;
+            // while (i < 6)
+            // {
+            //     Console.WriteLine(i);
+            //     i = i + 1;
+            // }
+
+            // create random numbers
+            Random rand = new Random();
+            for (int val = 0; val < 10; val++)
+            {
+                //Prints the next random value between 2 and 8
+                Console.WriteLine(rand.Next(2, 8));
+            }
+
+
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            // int end = 255;
+            int endHundo = 100;
+            string f = "Fizz";
+            string b = "Buzz";
+
+            // while (start <= end)
+            // {
+            //     Console.WriteLine(start);
+            //     start++;
+            // }
+
+            // while (start <= endHundo)
+            // {
+            //     if (start % 3 == 0)
+            //     {
+            //         if (start % 5 != 0)
+            //         {                        
+            //             Console.WriteLine(f);
+            //             start++;
+            //         } else if (start % 5 ==0){
+            //             Console.WriteLine(f+b);
+            //             start++;
+            //         }
+            //         start++;
+            //     }
+            //     else if (start % 5 == 0)
+            //     {
+            //         Console.WriteLine(b);
+            //         start++;
+            //     }
+            //     else
+            //     {
+            //         start++;
+            //     }
+            // }
+
+            for (int i = start; i <= endHundo; i++)
+            {
+                if (start % 3 == 0)
+                {
+                    if (start % 5 != 0)
+                    {
+                        Console.WriteLine(f);
+                        start++;
+                    }
+                    else if (start % 5 == 0)
+                    {
+                        Console.WriteLine(f + b);
+                        start++;
+                    }
+                    start++;
+                }
+                else if (start % 5 == 0)
+                {
+                    Console.WriteLine(b);
+                    start++;
+                }
+                else
+                {
+                    start++;
+                }
+            }
+        }
+    }
+}
